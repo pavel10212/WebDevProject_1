35e23134ac06b2abe68fcece39e6d28543239d00
Pavel Ponomarev <u6430176@au.edu>
2024-08-21 16:58:40 +0700

    Adjusting base

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
   plugins: [react()],
-});
+  base: "/WebDevProject-1/"
+})
